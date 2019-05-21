dac34bf9c805ef0ef6b19acb23e17f4e8731c1e2
MAtaur00 <mtmtarres@gmail.com>
2019-05-21 14:11:32 +0200

    Update
    - Score scene added
    - Score scene buttons done

---
+++ Assets/ScoreMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreMenu : MonoBehaviour {
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
